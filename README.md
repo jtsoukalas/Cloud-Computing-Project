e0176a8be5ebd6082b3699875e9edc42d14d1dff
EliteOneTube <manos1234599@gmail.com>
2024-01-26 20:40:32 +0200

    last changes + readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Cloud Computing Project
  
+### How to run
+1. Clone the repo `git clone https://github.com/jtsoukalas/Cloud-Computing-Project.git`
+2. Install the requirements `pip install -r requirements.txt`
+3. Run the app `python app.py`
+
+### Built With
+* [Python 3.9.1](https://www.python.org/downloads/release/python-391/) (Newer versions should work)
--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from library.controllers.scrape import scrape
-from library.utils.utils import Utils
-
-app = Flask(__name__)
-
-app.register_blueprint(scrape)
-Utils.cashing_urls()
-
 if __name__ == '__main__':
+    from flask import Flask
+    from library.controllers.scrape import scrape
+    from library.utils.utils import Utils
+
+    app = Flask(__name__)
+
+    app.register_blueprint(scrape)
+    Utils.cashing_urls()
+
     app.run()
